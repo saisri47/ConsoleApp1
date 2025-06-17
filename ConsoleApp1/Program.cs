f25ba2c50fd23c4e7932bbbdbbcee02f5b1952d3
saisri47 <bogapathisaisri@gmail.com>
2025-06-17 06:57:59 +0530

    keeping one message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy one more modification!"); Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-Console.WriteLine("Hello, C# Academy one more modification!");
-
+Console.WriteLine("Hello, C# Academy one more modification!"); 
